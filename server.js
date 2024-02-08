a8ba5e75aee04991be5f37a24cf1550470666e1b
S-Axhwin <s.ashwin.sathiyamoorthy.d@gmail.com>
2024-02-08 22:06:50 +0530

    again added cors

--- server.js
+++ server.js
@@ -1,11 +1,12 @@
 require("dotenv").config();
 const express = require('express');
 const app = express();
+const cors = require('cors')
 const connectDB = require('./utils/db');
 const authRouter = require('./routes/auth-route')
 app.use(express.json())
+app.use(cors)
 app.use('/api/auth', authRouter)
-
 app.post('/', (req, res)=>{
     console.log(req.body);
     res.json({mes: "welcome bro"})
