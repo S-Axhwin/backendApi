65255ccbe99ae2567fbe0efe11b7e3e1e5a65dea
S-Axhwin <s.ashwin.sathiyamoorthy.d@gmail.com>
2024-02-08 21:04:02 +0530

    update cors

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 require("dotenv").config();
 const express = require('express');
+const cors = require('cors')
 const app = express();
 const connectDB = require('./utils/db');
 const authRouter = require('./routes/auth-route')
