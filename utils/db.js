47590cfddeaa196246f1a832f2e54ee776d0bd2f
S-Axhwin <s.ashwin.sathiyamoorthy.d@gmail.com>
2024-02-08 20:19:19 +0530

    this is 5000 time i am changing

--- utils/db.js
+++ utils/db.js
@@ -4,7 +4,7 @@ const mongoose = require('mongoose');
 const connectDB = async (URI) =>{
     try {
         await mongoose.connect(URI)
-        console.log('connect to db');
+        console.log('connect to db i have done 5000 time on 5000 port');
     } catch (error) {
         console.log(error);
         console.error('this is not the correct way');
