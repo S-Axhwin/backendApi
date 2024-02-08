19646ebfaf278a2904f3301b7408400c8f478e8c
S-Axhwin <s.ashwin.sathiyamoorthy.d@gmail.com>
2024-02-08 22:21:18 +0530

    cors is not a property it is function

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const cors = require('cors')
 const connectDB = require('./utils/db');
 const authRouter = require('./routes/auth-route')
 app.use(express.json())
-app.use(cors)
+app.use(cors())
 app.use('/api/auth', authRouter)
 app.post('/', (req, res)=>{
     console.log(req.body);
