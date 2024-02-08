bad48130ed57987db266f9aaa56c077254fbb55b
S-Axhwin <s.ashwin.sathiyamoorthy.d@gmail.com>
2024-02-08 19:49:07 +0530

    added railway pkg

--- models/user-models.js
+++ models/user-models.js
@@ -3,11 +3,11 @@ const mongoose = require('mongoose');
 const userSchema = new mongoose.Schema({
     username: {
         type: String,
-        require: true,
+        required: true,
     },
     password: {
-        type: String ,
-        require: true
+        type : String ,
+        required : true
     },
     feed: {
         type: Object,
